f4b1bb3fa31bd2a3f168bf0ea5b012a514ce2f2c
Hermet Michael <hermet.mkl@gmail.com>
2026-02-21 16:31:08 +0100

    chore: adding react-datepicker chunk in config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from "rollup-plugin-visualizer";
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), visualizer({ open: true })],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, 'src/components'),
@@ -15,12 +16,13 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 500, // optional: only changes warning threshold
+    chunkSizeWarningLimit: 500,
     rollupOptions: {
       output: {
         manualChunks: {
           react: ['react', 'react-dom'],
           query: ['@tanstack/react-query'],
+          datepicker: ['react-datepicker'],
           // add other heavy libs here (charts, editor, etc.)
         },
       },
