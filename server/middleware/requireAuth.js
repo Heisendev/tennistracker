da56df22fbcd0086e97906c96886c9d651146869
Hermet Michael <hermet.mkl@gmail.com>
2026-02-24 17:42:05 +0100

    add log

--- server/middleware/requireAuth.js
+++ server/middleware/requireAuth.js
@@ -1,4 +1,5 @@
 export const requireAuth = (req, res, next) => {
+    console.log("req", req);
     if (!req.session?.userId) {
         return res.status(401).json({ error: 'Unauthorized' });
     }
