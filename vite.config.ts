c159374b66632373a7e11752b81aae740ac40eee
Hermet Michael <hermet.mkl@gmail.com>
2026-03-03 17:12:15 +0100

    set visualizer only on ANALYZE env

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,34 @@ import { defineConfig } from 'vite'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({ open: true })],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@types': path.resolve(__dirname, 'src/types'),
-      '@providers': path.resolve(__dirname, 'src/providers'),
-      '@hooks': path.resolve(__dirname, 'src/hooks'),
-      '@services': path.resolve(__dirname, 'src/services'),
+export default defineConfig(() => {
+  const analyzePlugins = process.env.ANALYZE === 'true' ? [visualizer({ open: true })] : []
+
+  return {
+    plugins: [react(), tailwindcss(), ...analyzePlugins],
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, 'src/components'),
+        '@pages': path.resolve(__dirname, 'src/pages'),
+        '@types': path.resolve(__dirname, 'src/types'),
+        '@providers': path.resolve(__dirname, 'src/providers'),
+        '@hooks': path.resolve(__dirname, 'src/hooks'),
+        '@services': path.resolve(__dirname, 'src/services'),
+      },
     },
-  },
-  build: {
-    chunkSizeWarningLimit: 500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          zod: ['zod'],
-          motion: ['motion-dom'],
-          query: ['@tanstack/react-query'],
-          datepicker: ['react-datepicker'],
-          // add other heavy libs here (charts, editor, etc.)
+    build: {
+      chunkSizeWarningLimit: 500,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ['react', 'react-dom'],
+            zod: ['zod'],
+            motion: ['motion-dom'],
+            query: ['@tanstack/react-query'],
+            datepicker: ['react-datepicker'],
+          },
         },
       },
     },
-  },
+  }
 })
